19ff90715296408c466d1de6f48c408696151d59
Angelin Calu <angelin.calu@gmail.com>
2020-12-19 00:43:41 +0200

    install tailwindcss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,37 @@
 import React from "react";
+import { graphql } from 'gatsby';
 import useSiteMetadata from '../hooks/useSiteMetadata';
 import Layout from '../layout';
 
-export default function Home() {
+export default function Home({ data }) {
   const { title, description } = useSiteMetadata();
   return <Layout>
             <h2>Welcome</h2>
             <h1>{title}</h1>
             <p>{description}</p>
+            {data.allMdx.nodes.map(({ frontmatter }) => (
+                  <div>
+                    <h1>{frontmatter.title}</h1>
+                    <h1>{frontmatter.description}</h1>
+                    <p>{frontmatter.date}</p>
+                  </div>
+                ))}
         </Layout>;
 };
+
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      nodes {
+        id
+        frontmatter {
+          title
+          description
+          date(formatString: "YYYY-MM-DD")
+        }
+      }
+    }
+  }
+`;
