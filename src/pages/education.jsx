4562b90e61b149e9e79d54ada769fb08852f3d09
Angelin Calu <angelin.calu@gmail.com>
2020-12-31 04:34:33 +0200

    filled in Education

--- src/pages/education.jsx
+++ src/pages/education.jsx
@@ -7,6 +7,128 @@ const Education = () => {
         <Layout>
             <SEO title="Education" />
             <h1 className="text-4xl py-4">Education</h1>
+            <h2 className="text-lg font-bold pt-4 pb-2">2001 - 2005</h2>
+            <p>
+                My Passion for the IT world started in the late 90's, and that's why I've chosen to continue my High
+                School studies in this field.
+            </p>
+            <p>
+                In High School (Mathematics and Informatics, Anghel Saligny, Braila, Romania) the curriculum covered
+                mostly programming in <b>C++</b>, and very basic front end stuff (like HTML), but because of my early
+                passion for web and software development, I've been involved in an ERASMUS exchange program called{' '}
+                <b className="text-gray-500 font-bold italic">"New Technologies - Multimedia and internet"</b>.
+            </p>
+            <p>I've also been awarded several times, in various local programming competitions.</p>
+            <h2 className="text-lg font-bold pt-4 pb-2">2006 - 2010</h2>
+            <p>
+                After High School, I decided to pursue a career in shipbuilding as that was probably the main activity
+                in the region.
+            </p>
+            <p>
+                I got my Bachelor's Degree in Naval Architecture (from Lower Danube University in Galati, Romania) in
+                the summer of 2010.
+            </p>
+            <p>
+                Besides the specific technical classes, the University curriculum also included programming in C++ and
+                VBA (in Microsoft Excel).
+            </p>
+            <h2 className="text-lg font-bold pt-4 pb-2">2010 - 2012</h2>
+            <p>In October 2010 I've been accepted into the Master's programme of the same University.</p>
+            <p>
+                In February 2011 I already started working full-time in the shipbuilding industry (while continuing the
+                studies).
+            </p>
+            <p>
+                In the summer of 2012 I've passed all the exams but unfortunately didn't managed to finish my Master's
+                thesis in time.
+            </p>
+
+            <h2 className="text-lg font-bold py-4">&nbsp;</h2>
+            <p>
+                The study period has definitely continued even after the institutionalized education. I've always been
+                able to find good resources to learn on my own, expecially in the field of software and web development.
+                And, to be perfectly honest, keeping up with the latest technologies and practices became a part of who I am.
+            </p>
+            <p>Some of those trainings (the ones that would provide some certificate of completion, 😊 below):</p>
+
+            <ul className="py-3 list-disc space-y-2">
+                <li>
+                    <b>Laracasts.com (by Jeffrey Way)</b>
+                    <p><span className="text-gray-500">Period: </span><span className="text-sm">25.08.2016 - Present time</span></p>
+                    <p><span className="text-gray-500">Topics: </span><span className="text-sm">A wide variety of topics like PHP, Laravel, MySQL, JavaScript, Clean Code, etc.</span></p>
+                    <p>
+                        <span className="text-gray-500">Organizer: </span>
+                        <span className="text-sm">
+                            <a
+                                title="Laracasts.com"
+                                className="font-bold hover:underline hover:text-red-700"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://laracasts.com/browse/all"
+                            >
+                                Laracasts.com
+                            </a>
+                        </span>
+                    </p>
+                </li>
+                <li>
+                    <b>Buutcamp Sprint: TypeScript</b>
+                    <p><span className="text-gray-500">Period: </span><span className="text-sm">27.08.2020 - 17.12.2020</span></p>
+                    <p><span className="text-gray-500">Topics: </span><span className="text-sm">TypeScript, React, ExpressJs, MongoDB, PostgreSQL, Agile / SCRUM methodology.</span></p>
+                    <p>
+                        <span className="text-gray-500">Organizer: </span>
+                        <span className="text-sm">
+                            <a
+                                title="Buutti Oy"
+                                className="font-bold hover:underline hover:text-red-700"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://www.buutti.com"
+                            >
+                                Buutti Oy, Finland
+                            </a>
+                        </span>
+                    </p>
+                </li>
+                <li>
+                    <b>Modern JavaScript for React JS - ES6.</b>
+                    <p><span className="text-gray-500">Period: </span><span className="text-sm">August 2020</span></p>
+                    <p><span className="text-gray-500">Topics: </span><span className="text-sm">JavaScript.</span></p>
+                    <p>
+                        <span className="text-gray-500">Organizer: </span>
+                        <span className="text-sm">
+                            <a
+                                title="Udemy"
+                                className="font-bold hover:underline hover:text-red-700"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://www.udemy.com/certificate/UC-ae82c1a4-57e9-4a69-9286-2ecacf61a671/"
+                            >
+                                Udemy
+                            </a>
+                        </span>
+                    </p>
+                </li>
+                <li>
+                    <b>Interactive Web Animation 2020 [JavaScript, SVG, CSS & HTML]</b>
+                    <p><span className="text-gray-500">Period: </span><span className="text-sm">December 2020</span></p>
+                    <p><span className="text-gray-500">Topics: </span><span className="text-sm">CSS, SVG and JavaScript animations.</span></p>
+                    <p>
+                        <span className="text-gray-500">Organizer: </span>
+                        <span className="text-sm">
+                            <a
+                                title="Udemy"
+                                className="font-bold hover:underline hover:text-red-700"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://www.udemy.com/certificate/UC-6cec81c3-a7d0-400a-9bd0-cf3c04cf5815/"
+                            >
+                                Udemy
+                            </a>
+                        </span>
+                    </p>
+                </li>
+            </ul>
         </Layout>
     );
 };
