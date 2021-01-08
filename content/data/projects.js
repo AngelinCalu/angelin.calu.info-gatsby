6ef3a5e1daa74f6e640b5d50879baffd20582674
Angelin Calu <angelin.calu@gmail.com>
2021-01-08 16:08:40 +0200

    added two projects

--- content/data/projects.js
+++ content/data/projects.js
@@ -6,4 +6,10 @@ module.exports = [
         image: 'angelin-calu-info.jpg',
         technologies: ['JavaScript', 'ReactJs', 'GatsbyJs'],
     },
+    {
+        title: 'pml extension for Visual Studio Code',
+        description: 'VSCode extension to provides syntax highlight for PML (Programmable Macro Language), code navigation, snippets and basic code completion.',
+        link: 'https://github.com/x10sv/pml',
+        technologies: ['TypeScript'],
+    },
 ];
--- src/pages/experience.jsx
+++ src/pages/experience.jsx
@@ -3,6 +3,8 @@ import Layout from '../layout';
 import SEO from '../components/seo';
 import { Link } from 'gatsby';
 
+import projects from '../../content/data/projects';
+
 const Experience = () => {
     return (
         <Layout>
@@ -12,15 +14,8 @@ const Experience = () => {
                 description="Portfolio of web development projects done by Angelin Calu, full stack developer based in Turku, Finland"
             />
             <h1 className="text-4xl py-4 dark:text-gray-100">Experience</h1>
-            <p>
-                A list of projects I've done (software / web development) will be soon posted here.
-                <br />
-                Although I have successfully completed several big projects (ERP / CRM systems), they have sensitive
-                data which should probably not be made public.
-            </p>
-
             <p className="py-2">
-                If you want to see a list of ships I've worked on, please check the list under{' '}
+                <strong>Note!</strong> If you want to see a list of ships I've worked on, please check the list under{' '}
                 <Link
                     className="hover:underline hover:text-blue-500"
                     title="Shipbuilding Projects"
@@ -30,6 +25,37 @@ const Experience = () => {
                 </Link>
                 .
             </p>
+
+            <p>
+                Although I have successfully completed several big projects (ERP / CRM systems, e-commerce shops, etc.), they have sensitive
+                data which should probably not be made public.
+            </p>
+
+
+            <p>Below some projects I can show (more to be added soon): </p>
+             <div className="py-4">
+                {
+                    projects.map(project => (
+                        <div key={project.title} className="p-2 my-2 rounded bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <h2 className="text-xl font-heading text-gray-900 dark:text-white p-2 text-center">{project.title}</h2>
+                            <div className="text-gray-600 dark:text-gray-300">
+                                <div className="my-2 grid grid-cols-1 md:grid-cols-2">
+                                    <span className="text-sm font-bold p-1 md:text-right mr-2">Link:</span>
+                                    <span><a target="_blank" className="hover:underline hover:text-blue-500 text-sm" href={project.link} rel="nofollow noopener noreferrer">{project.link}</a></span>
+                                </div>
+                                <div className="my-2 grid grid-cols-1 md:grid-cols-2">
+                                    <span className="text-sm font-bold p-1 md:text-right mr-2">Description:</span>
+                                    <span>{project.description}</span>
+                                </div>
+                                <div className="my-2 grid grid-cols-1 md:grid-cols-2">
+                                    <span className="text-sm font-bold p-1 md:text-right mr-2">Technologies used:</span>
+                                    <span><strong>{project.technologies.join(', ')}</strong></span>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         </Layout>
     );
 };
