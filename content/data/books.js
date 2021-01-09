5e4255b2f6fb18b05977cf658a37ca41300bec39
Angelin Calu <angelin.calu@gmail.com>
2021-01-09 21:17:51 +0200

    added skip to content link for screen readers

---
+++ content/data/books.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        title: 'Don\'t Make Me Think, Revisited: A Common Sense Approach to Web Usability (3rd Edition) (Voices That Matter)',
+        author: 'Steve Krug',
+        published: '2013-12-24',
+        tags: ['User Experience', 'Website Usability'],
+    },
+    {
+        title: 'Refactoring UI',
+        author: 'Adam Wathan, Steve Schoger',
+        published: '2018-12-11',
+        tags: ['User Experience', 'User Interfaces'],
+    },
+];
