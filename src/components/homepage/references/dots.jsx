57132be171f315593e34b7569017a5bbfe38cafc
Angelin Calu <angelin.calu@gmail.com>
2021-01-12 15:05:35 +0200

    styling WIP

--- src/components/homepage/references/dots.jsx
+++ src/components/homepage/references/dots.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 
 const Dots = ({ references, active }) => {
     return (
-        <div className="absolute bottom-2 left-1/2 my-2 inline-flex items-center space-x-4">
-            {references.map((reference) => (
-                <span key={reference} className="flex items-center relative">
-                    {reference === active ? <span className="absolute block opacity-75 w-full h-full bg-blue-500 animate-ping rounded-full" />: null} 
-                    <span
-                        className={`relative block w-3 h-3 rounded-full ${reference === active ? 'bg-blue-300' : 'bg-gray-400'}`}
-                    />
-                </span>
-            ))}
+        <div className="relative w-1/3 items-end my-2 ">
+            <div className="absolute bottom-4 left-1/2 inline-flex space-x-4">
+                {references.map((reference) => (
+                    <span key={reference} className="flex items-center relative">
+                        {reference === active ? (
+                            <span className="absolute block opacity-75 w-full h-full bg-blue-500 animate-ping rounded-full" />
+                        ) : null}
+                        <span
+                            className={`relative block w-3 h-3 rounded-full ${
+                                reference === active ? 'bg-blue-300' : 'bg-gray-400'
+                            }`}
+                        />
+                    </span>
+                ))}
+            </div>
         </div>
     );
 };
--- src/components/homepage/references/index.jsx
+++ src/components/homepage/references/index.jsx
@@ -4,36 +4,44 @@ import referencesData from '../../../../content/data/references';
 import Dots from './dots';
 
 const References = () => {
+    const refIds = referencesData.map((ref) => ref.id);
 
-    const refIds = referencesData.map(ref => ref.id);
-    
     const wrapperRef = useRef(null);
-    const getWidth = () => wrapperRef.current.clientWidth;
-    
+    const getWidth = () => (wrapperRef.current ? wrapperRef.current.clientWidth : 0);
+
     const [activeRef, setActiveRef] = useState(refIds[0]);
     const [translate, setTranslate] = useState(0);
-    
+
     const moveBackwards = () => {
-        console.log(getWidth());
-        setActiveRef(refIds.indexOf(activeRef) === 0 ? refIds[refIds.length - 1] : refIds[refIds.indexOf(activeRef) - 1]);
-        setTranslate(getWidth() * refIds.indexOf(activeRef) + 1);
-    }
-    
+        let nextId =
+            refIds.indexOf(activeRef) === 0 ? refIds[refIds.length - 1] : refIds[refIds.indexOf(activeRef) - 1];
+
+        setTranslate(-1 * getWidth() * refIds.indexOf(nextId) + 1 + 8 * refIds.indexOf(nextId));
+        setActiveRef(nextId);
+    };
+
     const moveForward = () => {
-        setActiveRef(refIds.indexOf(activeRef) === refIds.length - 1 ? refIds[0] : refIds[refIds.indexOf(activeRef) + 1]);
-        setTranslate(getWidth() * refIds.indexOf(activeRef) -1);
-    }
+        let nextId =
+            refIds.indexOf(activeRef) === refIds.length - 1 ? refIds[0] : refIds[refIds.indexOf(activeRef) + 1];
+
+        setTranslate(-1 * getWidth() * refIds.indexOf(nextId) - 1 + 8 * refIds.indexOf(nextId));
+        setActiveRef(nextId);
+    };
 
     return (
         <section className="py-6">
             <h3 className="uppercase text-2xl py-2">References</h3>
             <h4 className="text-md py-2">Here's some words from a few of the colleagues or collaborators:</h4>
-            <div ref={wrapperRef} className="w-full p-4 justify-between flex rounded-md group bg-gray-100 dark:bg-gray-800 overflow-hidden relative hover:shadow-md">
-                <div className="absolute z-10 w-full h-full -m-4">
+            <div
+                width={getWidth() + referencesData.length}
+                ref={wrapperRef}
+                className="w-full p-4 justify-between flex rounded-md group bg-gray-100 dark:bg-gray-800 overflow-hidden relative hover:shadow-md"
+            >
+                <div className="absolute z-10 w-full h-full -m-4 inline-flex">
                     <button
                         type="button"
-                        className="w-1/2 min-h-full bg-gradient-to-r from-gray-100 bg-opacity-50 opacity-0 hover:opacity-75 focus:outline-none text-left"
-                        onClick={ moveBackwards }
+                        className="w-1/3 min-h-full bg-gradient-to-r from-gray-100 bg-opacity-50 opacity-0 hover:opacity-75 focus:outline-none text-left"
+                        onClick={moveBackwards}
                     >
                         <span className="sr-only">Previous Reference</span>
                         <svg
@@ -50,9 +58,10 @@ const References = () => {
                             <path d="M15 18l-6-6 6-6" />
                         </svg>
                     </button>
+                    <Dots references={refIds} active={activeRef} />
                     <button
                         type="button"
-                        className="w-1/2 min-h-full bg-gradient-to-l from-gray-100 opacity-0 hover:opacity-75 focus:outline-none text-right"
+                        className="w-1/3 min-h-full bg-gradient-to-l from-gray-100 opacity-0 hover:opacity-75 focus:outline-none text-right"
                         onClick={moveForward}
                     >
                         <span className="sr-only">Next Reference</span>
@@ -75,7 +84,7 @@ const References = () => {
                     <blockquote
                         key={reference.id}
                         className="min-w-full relative z-0 m-2 flex flex-col items-end transform transition ease-out duration-700"
-                        style={{ transform: `translateX(-${translate - 16}px)` }}
+                        style={{ transform: `translateX(${translate ? translate + 20 : 0}px)` }}
                     >
                         <svg
                             className="absolute top-0 left-2 transform -translate-x-3 -translate-y-2 h-8 w-8 text-blue-300 opacity-50"
@@ -84,7 +93,7 @@ const References = () => {
                         >
                             <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z"></path>
                         </svg>
-                        <h2 className="relative text-lg p-2 grouregp-hover:text-blue-700 dark:group-hover:text-blue-300 text-center text-gray-500 dark:text-gray-300 italic">
+                        <h2 className="relative w-full text-lg p-2 grouregp-hover:text-blue-700 dark:group-hover:text-blue-300 text-center text-gray-500 dark:text-gray-300 italic">
                             &quot;{reference.quote}&quot;
                         </h2>
                         <p className="text-sm text-gray-700 dark:text-gray-200 w-full text-right mr-4 flex flex-col mt-4 py-2">
@@ -93,7 +102,6 @@ const References = () => {
                         </p>
                     </blockquote>
                 ))}
-                <Dots references={refIds} active={activeRef}/>
             </div>
         </section>
     );
