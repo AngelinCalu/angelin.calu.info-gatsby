a0201f0a941570004a601bfcea8530d13e1d17ad
Angelin Calu <angelin.calu@gmail.com>
2021-01-19 16:43:16 +0200

    added recommendation from Perttu

--- content/data/references.js
+++ content/data/references.js
@@ -2,19 +2,26 @@ module.exports = [
     {
         id: 1,
         quote:
+            'Angelin is an embodiment of agile software development. He’s a fast learner, supportive teammate and endlessly interested in modern web technologies. By being his teacher, I in turn learned valuable tidbits about web development.',
+        name: 'Perttu Tuovinen',
+        position: 'Software developer and ICT-instructor, Buutti Oy',
+    },
+    {
+        id: 2,
+        quote:
             'Besides his engineering responsibilities, Mr. Calu Angelin took initiative and developed and implemented several software solutions (involving his programming skills) which automated some of the tasks done to produce part of our deliverable technical documentation.',
         name: 'Petrica Necula',
         position: 'Engineering Director at Icepronav Engineering SRL',
     },
     {
-        id: 2,
+        id: 3,
         quote:
             'While working for our company the employee was assigned as Team Leader, being responsible for leading, supervising and coordinating a team of engineers and draftpersons, reporting directly to the Project Manager.',
         name: 'Petrica Necula',
         position: 'Engineering Director at Icepronav Engineering SRL',
     },
     {
-        id: 3,
+        id: 4,
         quote: 'Angelin Calu was a valuable engineer and I was pleased to have him in my department.',
         name: 'Popescu Daniel-Ciprian',
         position: 'Vice President Technical at Vard Braila',
